24aa37b664b5dee0fbaa5c9abae4999e23496115
fox67rus <fox67rus@mail.ru>
2016-06-09 09:37:00 +0300

    Комментарии

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
 // change the following paths if necessary
+//echo 'Текущая версия PHP: ' . phpversion();
 
+//формируем путь до файла настроек
 $config = $_SERVER['DOCUMENT_ROOT'] . '/SimpleEngine/config/main_cnfg.php';
-//var_dump($config);
 
+//подключаем файл настроек
 require_once $config;
 
+
+//создаем экземпляр приложения с помощью синглтон
 $app = \SimpleEngine\Controller\AppController::getInstance()->run();
 
