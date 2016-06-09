ac944a55ef9f8193459eccc4d0b34bee40b604cd
fox67rus <fox67rus@mail.ru>
2016-06-09 09:35:59 +0300

    Добавление тестового метода для формирования массива

--- Model/Page.php
+++ Model/Page.php
@@ -21,4 +21,10 @@ class Page
     public function getUser(){
         return $this->user;
     }
+
+    //тестовый метод для организации сбора каких либо данных в массив для дальнейшего вывода на экран
+    public function getArray(){
+        $s="1";
+        return $s;
+    }
 }
